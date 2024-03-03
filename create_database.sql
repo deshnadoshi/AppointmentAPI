59a16c2fa39aa1f52f9cff2201793873f4bf4b5d
deshnadoshi <deshna.doshi@gmail.com>
2024-03-02 20:18:43 -0500

    database creation query file

---
+++ create_database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE calendar;
+
+USE calendar;
+
+CREATE TABLE appointments (
+    attendee VARCHAR(255),
+    dtstart DATE,
+    dtstamp DATETIME,
+    method VARCHAR(255),
+    stat VARCHAR(255),
+    uid VARCHAR(255) PRIMARY KEY
+
+);
+
