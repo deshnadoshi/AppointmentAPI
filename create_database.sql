af206f2a34ae10291ada475dd9f31c3a663a2b52
deshnadoshi <deshna.doshi@gmail.com>
2024-03-03 22:19:36 -0500

    adjusted create_database

--- create_database.sql
+++ create_database.sql
@@ -12,3 +12,15 @@ CREATE TABLE appointments (
     UNIQUE KEY uniqueDtstart (dtstart)
 );
 
+-- Pre-loaded Entry 1
+INSERT INTO appointments (attendee, dtstart, dtstamp, method, stat, uid)
+VALUES ('preloaded1@gmail.com', '2024-09-25', '2024-03-02 00:00:00', 'REQUEST', 'CONFIRMED', 'A3xZ9k');
+
+-- Pre-loaded Entry 2
+INSERT INTO appointments (attendee, dtstart, dtstamp, method, stat, uid)
+VALUES ('preloaded2@yahoo.com', '2024-07-16', '2024-03-02 00:00:00', 'REQUEST', 'TENTATIVE', 'P7qR2y');
+
+-- Pre-loaded Entry 3
+INSERT INTO appointments (attendee, dtstart, dtstamp, method, stat, uid)
+VALUES ('preloaded3@outlook.com', '2025-05-14', '2024-03-02 00:00:00', 'REQUEST', 'TENTATIVE', '8645e2c5');
+
